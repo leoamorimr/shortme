e2d328f2b7f380822c7edad41a145dea989e7b6c
Leonardo Ramos <leonardo.a.ramos@accenture.com>
2024-08-25 19:53:24 +0100

    fix add cors config to ui

--- src/main/java/com/leonardoramos/shortme/config/ConfigCors.java
+++ src/main/java/com/leonardoramos/shortme/config/ConfigCors.java
@@ -13,7 +13,7 @@ public class ConfigCors {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8080", "http://54.73.218.229:8080")
+                        .allowedOrigins("http://localhost:8080", "http://54.73.218.229/*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
